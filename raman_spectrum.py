0658868b4bb8047430e86ac036ff046206738d6b
jacob-128514 <79486959+jacob-128514@users.noreply.github.com>
2021-12-16 19:27:09 -0700

    Create raman_spectrum.py

---
+++ raman_spectrum.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import scipy.signal as sp
+import matplotlib.pyplot as plt
+
+#USB2000 Ocean Optics Emulator from pypi.org/project/seatease/
+import seatease.spectrometers as s
+
+#Declaring spectrometer
+spec = s.Spectrometer.from_first_available()
+
+#For getting a specific spectrometer (in case it's not recognizing):
+#spec = s.Spectrometer.from_serial_numer("serial number")
+
+#Set integration time:
+spec.integration_time_micros(70*1000) # 70 ms
+
+#Converting wavelengths to waveshifts
+Laser_wavelength = 532
+Length = len(spec.intensities())
+intensities = np.array(spec.intensities())
+waveshifts = np.empty(Length)
+
+for i in range(0, len(spec.intensities())):
+    waveshifts[i] = ((1/Laser_wavelength)-(1/spec.wavelengths()[i]))*10**7
+
+#Storing intensities and waveshifts
+xvalues = np.array(waveshifts)
+yvalues = np.array(intensities)
+
+#Finding local maxima (within N data points to filter noise)
+N = 100
+ymaxes = sp.argrelextrema(yvalues, np.greater, order = N)
+
+#Formatting and displaying plot
+plt.plot(xvalues,yvalues)
+plt.scatter(xvalues[ymaxes], yvalues[ymaxes], c = 'g')
+plt.title('Generic Spectrum')
+plt.xlabel("Raman Shift ($\mathregular{cm^{-1}}$)")
+plt.ylabel("Intensity")
+plt.show()
